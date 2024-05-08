4f97c0114fddc4fb464b510bdb40f087826ea6c1
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-05-08 19:07:10 +0900

    [test] #2 - SoilMoisture test

---
+++ test/SoilMoisture.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import spidev
+import time
+
+delay = 1 
+
+# Open Spi Bus
+# SPI bus and device
+# max speed 1MHz
+spi = spidev.SpiDev()
+
+# check spi pin num (ls /dev/spi*)
+spi.open(0,0) # open(bus, device)
+spi.max_speed_hz = 500000 # set transfer speed
+
+# To read SPI data from MCP3008 chip
+# Channel must be 0~7 integer
+def readChannel(channel):
+  val = spi.xfer2([1, (8+channel)<<4, 0])
+  data = ((val[1]&3) << 8) + val[2]
+  return data
+
+# 0~1023 value? ???. 1023? ???? min?
+def convertPercent(data):
+  return 100.0-round(((data*100)/float(1023)),1)
+
+try:
+  while True:
+    val = readChannel(0)
+    if (val != 0) : # filtering for meaningless num
+      print("1 : ", val, "/", convertPercent(val),"%")
+    else:
+      print("1 : no data")
+    val = readChannel(1)
+    if (val != 0) : # filtering for meaningless num
+      print("2 : ", val, "/", convertPercent(val),"%")
+    else:
+      print("2 : no data")
+    time.sleep(delay)
+except KeyboardInterrupt:
+  spi.close()
+  print("Keyboard Interrupt!!!!")
