a1df934e4dc248a2a5493efae33f8b19ccf04efc
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-06-05 16:05:04 +0900

    [bug] #12 GPIO load error (ongoing)

--- webServer/GPIOtest.py
+++ webServer/GPIOtest.py
@@ -1,12 +1,36 @@
 from flask import Flask
 import RPi.GPIO as GPIO
+import threading
+import time
+import atexit
 
 app = Flask(__name__)
+
+def initialize_GPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17,GPIO.OUT)
+
+def cleanup():
+    GPIO.cleanup()
+
+atexit.register(cleanup)
+
+@app.route('/on')
+def ligthOn():
+    GPIO.output(17,True)
+    return "Lingt On!"
+
+@app.route('/off')
+def ligthOff():
+    GPIO.output(17,False)
+    return "Lingt Off!"
 
 @app.route('/')
 def main():
     return 'Hello, World!'
 
+
 if __name__ == '__main__':
+    initialize_GPIO()
     app.debug = True
-    app.run(port="3000", host='0.0.0.0')
+    app.run(host="0.0.0.0", port="3000") # port open
--- webServer/main.py
+++ webServer/main.py
@@ -1,58 +1,12 @@
 from flask import Flask
 import RPi.GPIO as GPIO
-import threading
-import time
-import atexit
 
 app = Flask(__name__)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
-
-running = threading.Event()
-running.set()
-terminate = threading.Event()
-
-def cleanup():
-    GPIO.cleanup()
-atexit.register(cleanup)
-
-def background_task():
-    while not terminate.is_set():
-        running.wait()
-        GPIO.output(17,True)
-        time.sleep(1);
-        GPIO.output(17,False)
-        time.sleep(1);
 
 @app.route('/')
 def main():
     return 'Hello, World!'
 
-@app.route('/on')
-def ligthOn():
-    #GPIO.output(17,True)
-    running.set()
-    return "Lingt On!"
-
-@app.route('/off')
-def ligthOff():
-    #GPIO.output(17,False)
-    running.clear()
-    return "Lingt Off!"
-
-@app.route('/stop')
-def lightStop():
-    running.set()
-    terminate.set()
-    bg_thread.join()
-    return "background thread stopped"
-
-bg_thread = threading.Thread(target=background_task)
-# bg_thread.daemon = True
-# bg_thread.start()
-
-
-
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="127.0.0.1", port="3000")
+    app.run(port="3000", host='0.0.0.0')
