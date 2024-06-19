18b5e96ccbb54c603c4c6ca6aacc50cfe8740a84
minzero31 <77minyoung77@gmail.com>
2024-06-19 22:12:17 +0900

    [feature] #12 web organization

--- webServer/main.py
+++ webServer/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import RPi.GPIO as GPIO
 import threading
 import time
@@ -6,11 +7,61 @@ import atexit
 import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 최대 파일 크기 설정 (16MB)
 
 def cleanup():
     GPIO.cleanup()
 
 atexit.register(cleanup)
+
+# 홈 페이지 라우트
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+# 마이 페이지 라우트
+@app.route('/mypage')
+def mypage():
+    return render_template('mypage.html')
+
+# 설정 페이지 라우트
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+# 모니터링 페이지 라우트
+@app.route('/monitoring')
+def monitoring():
+    return render_template('monitoring.html')
+
+# 일지 작성 페이지 라우트
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    if request.method == 'POST':
+        date = request.form['date']
+        plant = request.form['plant']
+        length = request.form['length']
+        status = request.form['status']
+        notes = request.form['notes']
+        photo = request.files['photo']
+
+        if photo:
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        log_entry = {
+            "date": date,
+            "plant": plant,
+            "length": length,
+            "status": status,
+            "notes": notes,
+            "photo": filename if photo else None
+        }
+
+        return render_template('log.html', log_entry=log_entry)
+
+    return render_template('log.html')
 
 @app.route('/setting')
 def setting():
@@ -27,11 +78,6 @@ def ligthOn():
 def ligthOff():
     GPIO.output(17,False)
     return "Lingt Off!"
-
-@app.route('/')
-def main():
-    return 'Hello, World!'
-
 
 if __name__ == '__main__':
     app.debug = True
